74ee2088ce468262669c76ecfeb8d101756d1517
GuruTesmaBhasker <gurubasker2005@gmail.com>
2025-04-19 19:53:06 +0530

    Add files via upload

---
+++ Escape sequence.py
@@ -0,0 +1,7 @@
+print('It\'s a beautiful day!')     # \' allows single quote inside single quotes
+print("It's a beautiful day!")      # You can even use like this using ' under " is consider as normal text 
+print("He said, \"Hello!\"")        # \" allows double quotes inside double quotes
+print('He said,"Hello!"')           # You can even use like this using " under ' is consider as normal text 
+print("Line 1\nLine 2")             # \n adds a new line
+print("Tab\tSpace")                 # \t adds a tab space
+print("Backslash: \\")              # \\ prints a backslash 
