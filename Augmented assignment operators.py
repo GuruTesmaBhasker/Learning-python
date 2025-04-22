6b179c2ee7a1d4d922289df337504571668ae10c
Guru T B <gurubasker2005@gmail.com>
2025-04-22 21:35:02 +0530

    Add files via upload

---
+++ Augmented assignment operators.py
@@ -0,0 +1,30 @@
+# Initialize x
+x = 10
+
+# Addition Assignment
+x += 5
+print("After addition assignment (x += 5):", x) # x = x+5 both are same
+
+# Subtraction Assignment
+x -= 3
+print("After subtraction assignment (x -= 3):", x) # x = x-5 both are same
+
+# Multiplication Assignment
+x *= 2
+print("After multiplication assignment (x *= 2):", x)# x = x*5 both are same
+
+# Division Assignment
+x /= 4
+print("After division assignment (x /= 4):", x) # x = x/5 both are same
+
+# Floor Division Assignment
+x //= 3
+print("After floor division assignment (x //= 3):", x)# x = x+//5 both are same
+
+# Modulus Assignment
+x %= 3
+print("After modulus assignment (x %= 3):", x) # x = x%5 both are same
+
+# Exponentiation Assignment
+x **= 3
+print("After exponentiation assignment (x **= 3):", x)  # x = x**5 both are same
