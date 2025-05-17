657d20674238a5ce95118289624154c3c4ca654a
Guru T B <gurubasker2005@gmail.com>
2025-05-17 12:14:47 +0530

    Add files via upload

---
+++ Expression in python.py
@@ -0,0 +1,35 @@
+# Expression
+# 4x2 + y - 5z 
+
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+z = int(input('Enter z: '))
+
+val = 4 * (x ** 2) + y - (5 * z)
+print(val)
+
+# Expression Evaluation
+
+"""
+if the given number is 
+
+
+x = 23
+y = 1235
+z = 45
+
+Step-by-step
+
+x^2 = 23^2 = 529
+4 x 529 = 2116
+5 x 45 = 225
+
+Now the final expression:
+2116 + 1235 - 225
+
+First:
+2116 + 1235 = 3351
+
+Then:
+3351 - 225 = 3126
+"""
