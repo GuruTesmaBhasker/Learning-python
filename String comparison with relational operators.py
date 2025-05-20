3be5165933656517ab9ba3f16727177a549238d2
Guru T B <gurubasker2005@gmail.com>
2025-05-20 13:56:58 +0530

    Add files via upload

---
+++ String comparison with relational operators.py
@@ -0,0 +1,23 @@
+str1 = "apple"
+str2 = "apple"
+print("str1 == str2:", str1 == str2)     # "apple" == "apple" → True
+
+str1 = "grape"
+str2 = "apple"
+print("str1 != str2:", str1 != str2)     # "grape" != "apple" → True
+
+str1 = "mango"
+str2 = "banana"
+print("str1 > str2:", str1 > str2)       # "mango" > "banana" → True (because 'm' > 'b') M has Ascii value and b has seperate Ascii value
+
+str1 = "apple"
+str2 = "zebra"
+print("str1 < str2:", str1 < str2)       # "apple" < "zebra" → True (because 'a' < 'z') same like the greater than operator
+
+str1 = "kiwi"
+str2 = "kiwi"
+print("str1 >= str2:", str1 >= str2)     # "kiwi" >= "kiwi" → True
+
+str1 = "cherry"
+str2 = "cherry"
+print("str1 <= str2:", str1 <= str2)     # "cherry" <= "cherry" → True
