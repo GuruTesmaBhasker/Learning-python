9018813fa513d2fbfa292a535cb123127bf73b04
Guru T B <gurubasker2005@gmail.com>
2025-05-20 13:55:49 +0530

    Add files via upload

---
+++ Finding average.py
@@ -0,0 +1,11 @@
+#finding average
+# Getting numbers from user
+mark1 = float(input("Enter first number: "))
+mark2 = float(input("Enter second number: "))
+mark3 = float(input("Enter third number: "))
+
+# Finding the average
+average = (mark1 + mark2 + mark3) / 3 
+
+# Printing the average
+print("The average is:", average)
