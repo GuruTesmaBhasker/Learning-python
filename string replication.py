60476b500572040de476118296264bfa856e4556
Guru T B <gurubasker2005@gmail.com>
2025-05-21 19:29:34 +0530

    Add files via upload

---
+++ string replication.py
@@ -0,0 +1,18 @@
+#string replication 
+a = "Hello "
+b = "World "
+c = "Python"
+
+print(a * 3)   # Hello Hello Hello 
+print(b * 2)   # World World 
+print(c)       # Python
+
+#string replication * and string concatenation +
+a = "Hi"
+b = "Bye"
+c = "Hello"
+d = "Python"
+
+print(a * 2 + b * 3)             # HiHi-ByeByeBye
+print(c * 2 + "-")               # HelloHello-
+print(d * 4)                     # PythonPythonPythonPython
