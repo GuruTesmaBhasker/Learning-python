9a94f1b835fb7b571b81fdbdb65e60fa90d1fd88
Guru T B <gurubasker2005@gmail.com>
2025-05-22 15:15:10 +0530

    Add files via upload

---
+++ Getting input from user.py
@@ -0,0 +1,16 @@
+# Getting string input
+text = input("Enter a word: ")
+print(text)
+#print(text*8) if i give like this will make string replication, if the input is a the OP is aaaaaaaa
+print(type(text))
+# It usually saves as string so we need to specify the data types or otherwise it saves as string and prints it
+
+# Getting integer input
+number = int(input("Enter an integer: ")) #This converts the input as integer
+print(number)
+print(type(number))
+
+# Getting float input
+decimal = float(input("Enter a decimal number: ")) #This converts the input as float
+print(decimal)
+print(type(decimal))
